9d8007a3b8779ab65fca2026372bb98153da4fa0
Racksoup <rack.connor1@gmail.com>
2022-05-11 13:54:30 -0400

    Create Services Section

--- src/UI/Components/Services/Services.jsx
+++ src/UI/Components/Services/Services.jsx
@@ -4,7 +4,16 @@ import './services.scss';
 const Services = ({ Ref }) => {
   return (
     <div className='Services' ref={Ref}>
-      Services
+      <div className='Title'>SERVICES</div>
+      <div className='TitleDesc'>
+        Lorem ipum dolor sit amet, consecteturr adipiscing elit, sed do eiusmod tempor incididunt ut
+        labore et dalore magna aliqua. Venenatis a
+      </div>
+      <div className='Content'>
+        <div className='Box-Red'></div>
+        <div className='Box-Blue'></div>
+        <div className='Box-Green'></div>
+      </div>
     </div>
   );
 };
