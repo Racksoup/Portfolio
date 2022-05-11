7f6cc8ff292e51ce69c60a02759bb3ef6e5e653e
Racksoup <rack.connor1@gmail.com>
2022-05-11 15:41:22 -0400

    About Created

--- src/UI/Components/About/About.jsx
+++ src/UI/Components/About/About.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
 import './about.scss';
 
+import HeaderBG from '../../../images/HeaderBG.png';
+
 const About = ({ Ref }) => {
   return (
     <div className='About' ref={Ref}>
-      About
+      <div className='Title'>ABOUT</div>
+      <div className='Contents'>
+        <img className='Img' src={HeaderBG} />
+        <div className='Text'>
+          FDSFD fdsfds fdsfds fdsafdsafd fdsfdf ere wr d sfd sfds fsdf dsfFDSFD fdsfds fdsfds
+          fdsafdsafd fdsfdf ere wr d sfd sfds fsdf dsfFDSFD fdsfds fdsfds fdsafdsafd fdsfdf ere wr d
+          sfd sfds fsdf dsfFDSFD fdsfds fdsfds fdsafdsafd fdsfdf ere wr d sfd sfds fsdf dsfFDSFD
+          fdsfds fdsfds fdsafdsafd fdsfdf ere wr d sfd sfds fsdf dsfFDSFD fdsfds fdsfds fdsafdsafd
+          fdsfdf ere wr d sfd sfds fsdf dsfFDSFD fdsfds fdsfds fdsafdsafd fdsfdf ere wr d sfd sfds
+          fsdf dsfFDSFD fdsfds fdsfds fdsafdsafd fdsfdf ere wr d sfd sfds fsdf dsfFDSFD fdsfds
+          fdsfds fdsafdsafd fdsfdf ere wr d sfd sfds fsdf dsfFDSFD fdsfds fdsfds fdsafdsafd fdsfdf
+          ere wr d sfd sfds fsdf dsf
+        </div>
+      </div>
     </div>
   );
 };
