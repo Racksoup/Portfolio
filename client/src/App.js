0ff5a58d26a12d0406a44f56fcb2eb5feace34d7
Racksoup <rack.connor1@gmail.com>
2022-08-13 12:47:26 -0400

    Works

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import './App.scss';
 import Home from './UI/Pages/Home/Home.jsx';
-import AdminLogin from './UI/Pages/AdminLogin/AdminLogin.jsx';
-import UserLogin from './UI/Pages/UserLogin/UserLogin.jsx';
-import CreateUser from './UI/Pages/CreateUser/CreateUser.jsx';
 import Navbar from './UI/Components/Navbar/Navbar.jsx';
 import Footer from './UI/Components/Footer/Footer.jsx';
 
@@ -17,9 +14,6 @@ const App = () => {
         <Navbar />
         <Routes>
           <Route exact path='/' element={<Home />} />
-          <Route exact path='/admin-login' element={<AdminLogin />} />
-          <Route exact path='/user-login' element={<UserLogin />} />
-          <Route exact path='/create-user' element={<CreateUser />} />
         </Routes>
         <Footer />
       </div>
